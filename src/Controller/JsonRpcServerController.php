1ef9537082c71a36834615315d44f4e9154d47cc
Kiril Savchev <k.savchev@gmail.com>
2025-11-03 14:17:37 +0200

    Init bundle services

--- src/Controller/JsonRpcServerController.php
+++ src/Controller/JsonRpcServerController.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 namespace Alcedo\Bundle\JsonRpcServerBundle\Controller;
 
+use Alcedo\JsonRpc\Server\DTO\Error;
+use Alcedo\JsonRpc\Server\DTO\ErrorCodes;
+use Alcedo\JsonRpc\Server\Factory\ErrorFactory;
+use Alcedo\JsonRpc\Server\Server as JsonRpcServer;
+use Alcedo\JsonRpc\Server\DTO\Response as JsonRpcResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class JsonRpcServerController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(JsonRpcServer $server, Request $request): Response
     {
-        return $this->json([]);
+        try {
+            $content = json_decode($request->getContent(), true, flags: JSON_THROW_ON_ERROR);
+            $result = $server->executeArrayRequest($content);
+        } catch (\JsonException $exception) {
+            $result = new JsonRpcResponse(error: ErrorFactory::invalidRequest(message: $exception->getMessage()));
+        } catch (\Throwable $exception) {
+            $error = new Error(ErrorCodes::INTERNAL_ERROR->value, $exception->getMessage());
+            $result = new JsonRpcResponse(error: $error);
+        }
+
+        return $this->json(json_encode($result));
     }
 }
